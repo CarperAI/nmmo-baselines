332a39c60d3a0d4758e361a802a4e9b73fdac923
David Bloomin <daveey@gmail.com>
2023-05-22 21:51:13 -0700

    ready for release

--- scripts/evaluate_policies.sh
+++ scripts/evaluate_policies.sh
@@ -2,7 +2,6 @@
 
 python -u -O -m tools.evaluate \
 --model.policy_pool=/fsx/home-daveey/experiments/pool.json \
---env.num_maps=100 \
 --env.num_npcs=256 \
 --env.death_fog_tick=100 \
 --eval.num_rounds=1000000 \
