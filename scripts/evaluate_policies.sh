4e91d14895428e72f4ab0e1a9e6cab68c069d2f8
David Bloomin <daveey@gmail.com>
2023-05-23 12:05:36 -0700

    negative logits

--- scripts/evaluate_policies.sh
+++ scripts/evaluate_policies.sh
@@ -3,7 +3,6 @@
 python -u -O -m tools.evaluate \
 --model.policy_pool=/fsx/home-daveey/experiments/pool.json \
 --env.num_npcs=256 \
---env.death_fog_tick=100 \
 --eval.num_rounds=1000000 \
 --eval.num_policies=8 \
 "${@}"
