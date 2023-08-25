fa60b41329be816b2bd13730ae2a615d8545294d
kywch <choe.kyoung@gmail.com>
2023-08-25 16:26:47 +0000

    refined wandb stats, stable config

--- environment.py
+++ environment.py
@@ -1,11 +1,12 @@
 from argparse import Namespace
 from collections import defaultdict
+import math
 
 import nmmo
 import pufferlib
 import pufferlib.emulation
 
-from leader_board import StatPostprocessor, score_unique_events
+from leader_board import StatPostprocessor
 
 class Config(nmmo.config.Default):
     """Configuration for Neural MMO."""
@@ -35,12 +36,14 @@ class Postprocessor(StatPostprocessor):
       replay_save_dir=None,
       sqrt_achievement_rewards=False,
       heal_bonus_weight=0,
-      explore_bonus_weight=0
-      ):
+      explore_bonus_weight=0,
+      clip_unique_event=3,
+    ):
         super().__init__(env, teams, team_id, replay_save_dir)
         self.sqrt_achievement_rewards = sqrt_achievement_rewards
         self.heal_bonus_weight = heal_bonus_weight
         self.explore_bonus_weight = explore_bonus_weight
+        self.clip_unique_event = clip_unique_event
 
     def reset(self, team_obs, dummy=False):
         super().reset(team_obs, dummy)
@@ -81,10 +84,13 @@ class Postprocessor(StatPostprocessor):
         healing_bonus = self.heal_bonus_weight if health_restore > 0 else 0
 
         # Unique event-based rewards, similar to exploration bonus
-        # NOTE: this gets slower as the size of event log increases
-        log = self.env.realm.event_log.get_data(agents=self.teams[self.team_id])
-        explore_bonus = self.explore_bonus_weight * score_unique_events(self.env.realm, log,
-          sqrt_rewards=self.sqrt_achievement_rewards)
+        # The number of unique events are available in self._curr_unique_count, self._prev_unique_count
+        if self.sqrt_achievement_rewards:
+            explore_bonus = math.sqrt(self._curr_unique_count) - math.sqrt(self._prev_unique_count)
+        else:
+            explore_bonus = min(self.clip_unique_event,
+                                self._curr_unique_count - self._prev_unique_count)
+        explore_bonus *= self.explore_bonus_weight
 
         team_reward = sum(team_rewards.values()) + explore_bonus + healing_bonus
 
