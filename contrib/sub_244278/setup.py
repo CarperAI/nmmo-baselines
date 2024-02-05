b7013e6e4bc29a9b823c32879c284b0bfcbace43
kywch <choe.kyoung@gmail.com>
2024-02-05 10:51:18 -0800

    added 244278

---
+++ contrib/sub_244278/setup.py
@@ -0,0 +1,55 @@
+from train_helper import TrainHelper, get_config_args, BASELINE_CURRICULUM_FILE
+from pufferlib.frameworks import cleanrl
+
+SUBMISSION_ID = "244278"
+
+def get_train_helper(debug=False):
+    run_prefix = f"s{SUBMISSION_ID}"
+    from reinforcement_learning import clean_pufferl, policy
+    from . import environment, config
+
+    args = get_config_args(config, BASELINE_CURRICULUM_FILE, debug)
+
+    def make_policy(envs):
+        learner_policy = policy.Baseline(
+            envs.driver_env,
+            input_size=args.input_size,
+            hidden_size=args.hidden_size,
+            task_size=args.task_size
+        )
+        return cleanrl.Policy(learner_policy)
+
+    policy_file = "reinforcement_learning/policy.py"
+    with open(policy_file, "r") as f:
+        policy_src = f.read()
+
+    # NOTE: For the PvP eval to work correctly, any custom components, like network blocks, etc,
+    #       must be included in the policy file.
+    policy_src += f"""
+
+class Config(nmmo.config.Default):
+    PROVIDE_ACTION_TARGETS = True
+    PROVIDE_NOOP_ACTION_TARGET = True
+    MAP_FORCE_GENERATION = False
+    TASK_EMBED_DIM = 4096
+    COMMUNICATION_SYSTEM_ENABLED = False
+
+def make_policy():
+    from pufferlib.frameworks import cleanrl
+    env = pufferlib.emulation.PettingZooPufferEnv(nmmo.Env(Config()))
+    # Parameters to your model should match your configuration
+    learner_policy = Baseline(
+        env,
+        input_size={args.input_size},
+        hidden_size={args.input_size},
+        task_size=4096
+    )
+    return cleanrl.Policy(learner_policy)
+
+"""
+
+    return TrainHelper(run_prefix, args,
+                       environment.make_env_creator,
+                       make_policy,
+                       clean_pufferl.CleanPuffeRL,
+                       policy_src)
